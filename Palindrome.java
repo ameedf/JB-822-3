4a34d0aadf8ead15300b6d62695981ad183c4656
Alex <alexander.ba@gmail.com>
2017-10-08 17:03:32 +0300

    Init

--- Palindrome.java
+++ Palindrome.java
@@ -23,8 +23,68 @@
  *    First part = 9 8 4 3      (with a space between each 2 characters)
  *    Second part = 3 4 8 9     (with a space between each 2 characters)
  */
+
+import java.util.Scanner;
+
 public class Palindrome {
-    public static void main(String[] args) {
-        // Your code goes here :)
-    }
+	public static void main(String[] args) {
+		int PalindromeNum, tmp, ogg, div = 0, PalindromeLen = 0;
+		Scanner in = new Scanner(System.in);
+		String outMessage;
+
+		do {
+			System.out.print("Enter number from 0 to 1,000,000,000:");
+			PalindromeNum = in.nextInt();
+		} while (PalindromeNum < 0 || PalindromeNum > 1000000000);
+		
+		//Find length of integer
+		tmp = PalindromeNum;
+		while (tmp != 0) {
+			tmp = tmp / 10;
+			PalindromeLen++;
+		}
+		
+		//Allocate an array depending on integer length
+		byte[] PalindromeArr = new byte[PalindromeLen];
+		
+		//Copy integer to array by characters
+		tmp = PalindromeNum;
+		for (int i = PalindromeArr.length - 1; i >= 0; i--) {
+			PalindromeArr[i] = (byte) (tmp % 10);
+			tmp /= 10;
+		}
+		
+		//Check if the entered integer is palindrome
+		boolean isPalindrome = true;
+		for (int i = 0, j = (PalindromeArr.length - 1); i < j && isPalindrome; i++, j--) {
+			if (PalindromeArr[i] != PalindromeArr[j]) {
+				isPalindrome = false;
+			}
+		}
+		
+		//Preparing and print out message 
+		if (isPalindrome) {
+			int N = PalindromeArr.length;
+			String firstPart = "";
+			String secondPart = "";
+			
+			for (int i = 0; i < PalindromeArr.length; i++) {
+				if (i < N / 2) {
+					firstPart += " " + PalindromeArr[i];
+				} else {
+					if ((N % 2 != 0) && (i == N / 2)) {
+						firstPart += " " + PalindromeArr[i];
+					}
+					secondPart += " " + PalindromeArr[i];
+				}
+			}
+			outMessage = "\nInput: " + PalindromeNum + "\nOutput: size = " + PalindromeArr.length + "\nFirst part ="
+					+ firstPart + "\nSecond part =" + secondPart;
+			System.out.println(outMessage);
+		} else {
+			System.out.println("\nEntered integer: " + PalindromeNum + " is NOT a Palindrome!");
+		}
+		
+		in.close();
+	}
 }
