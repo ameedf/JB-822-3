14f526eb8fb5523ba932c9acf7dfa0549e09bb69
Miri Bar <bar.mira.g@gmail.com>
2017-10-10 00:20:40 +0300

    Grades from Miri (revised)

--- Grades.java
+++ Grades.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Write a program that will compute some statistics about students grades.
  * It will ask the user to enter the number of students.
@@ -13,6 +15,55 @@
 public class Grades {
 
     public static void main(String[] args) {
-        // enter your code here :)
+
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Please enter the number of students (up to 127 allowed): ");
+        byte numOfStudents = input.nextByte();
+
+        if (numOfStudents <= 0) {
+            System.out.println("The number of students must be positive.");
+            return;
+        }
+
+        byte studentsArray[] = new byte[numOfStudents];
+        byte indexHighest = 0;
+        byte indexLowest = 0;
+
+        byte highest = 0;
+        byte lowest = 100;
+        float sum = 0;
+
+        System.out.println("Please enter the grades (each in a new line) - ");
+
+        for (byte i=0 ; i < studentsArray.length ; i++) {
+
+            System.out.printf("Enter grade: ");
+            short grade = input.nextShort();
+
+            while (grade < 0 || grade > 100) {
+                System.out.printf("Please enter a valid grade between 0 - 100: ");
+                grade = input.nextShort();
+            }
+
+                if (highest < grade) {
+                    highest = (byte)grade;
+                    indexHighest = i;
+                }
+                if (lowest > grade) {
+                    lowest = (byte)grade;
+                    indexLowest = i;
+                }
+                studentsArray[i] = (byte)grade;
+                sum += grade;
+            }
+
+        input.close();
+
+        indexHighest++;
+        indexLowest++;
+
+        System.out.println("Student #" + indexHighest + " got the highest grade of " + highest);
+        System.out.println("Student #" + indexLowest + " got the lowest grade of " + lowest);
+        System.out.println("Average of all grades is: " + sum/numOfStudents);
     }
 }
