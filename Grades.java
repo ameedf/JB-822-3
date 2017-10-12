f52fe29d0092f8575ecfde6d1dc92b35f18c012b
KhaliKasabri <32670821+KhaliKasabri@users.noreply.github.com>
2017-10-12 22:36:54 +0300

    Update Grades.java

--- Grades.java
+++ Grades.java
@@ -4,6 +4,7 @@ public class Grades {
     public static void main(String[] args) {
         public static void main(String[] args) {
 
+		// ask for number of grades
 		Scanner in = new Scanner(System.in);
 		System.out.println("How many grades would you like to enter?");
 		// user input how many grades user would like to enter
@@ -13,6 +14,7 @@ public class Grades {
 
 		System.out.println("Please enter the grades. Each between 0 an 100");
 
+		// checks grades' validity
 		int c = 0;
 		while (c < n) {
 
@@ -43,11 +45,13 @@ public class Grades {
 
 		while (a < n) {
 
+			// Finds lowest grade
 			if (lwst > grades[a]) {
 				lwst = grades[a];
 				lwstPos = a;
 			}
 
+			// Finds highest grade
 			sum = sum + grades[a];
 
 			if (hst < grades[a]) {
@@ -61,7 +65,9 @@ public class Grades {
 			}
 
 		}
+		//Callculates Average
 		avg = sum / n;
+		
 		System.out.println("The highest grade is " + hst + " and it's position in the array is " + hstPos);
 		System.out.println("The lowest grade is " + lwst + ", and it's position in the array is " + lwstPos);
 		System.out.println("The total avarage is " + avg + ".");
