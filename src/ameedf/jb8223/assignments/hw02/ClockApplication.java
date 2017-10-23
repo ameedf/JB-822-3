6b6b6621b820d9172e905e5a74ceedef85310670
Alex <alexander.ba@gmail.com>
2017-10-23 13:30:36 +0300

    add Bonus2

--- src/ameedf/jb8223/assignments/hw02/ClockApplication.java
+++ src/ameedf/jb8223/assignments/hw02/ClockApplication.java
@@ -1,10 +1,11 @@
 package ameedf.jb8223.assignments.hw02;
 import ameedf.jb8223.assignments.hw02.Clock;
 import ameedf.jb8223.assignments.hw02.ClockBonus1;
+import ameedf.jb8223.assignments.hw02.ClockBonus2;
 
 public class ClockApplication { 
     public static void main(String[] args) {
-
+/*
     		Clock timer1 = new Clock();
     		timer1.print(false);
     		timer1.print(true);
@@ -22,18 +23,39 @@ public class ClockApplication {
     		timer3.print(false);
     		timer3.addMilliseconds(-1);
     		timer3.print(false);
-//   	timer3.addHours(-1);
-//  		timer3.addMinutes(-90);
-//    	timer3.print(false);
-//   	timer3.addSeconds(-60);
-//   	timer3.addMilliseconds(-1000);
+     	timer3.addHours(-1);
+  		timer3.addMinutes(-90);
+      	timer3.print(false);
+     	timer3.addSeconds(-60);
+     	timer3.addMilliseconds(-1000);
     		timer3.addMinutes(90);
     		timer3.print(false);
     		timer3.addMinutes(-30);
     		timer3.print(false);
     		timer3.addMinutes(-121);
     		timer3.print(false);
+ */   		
+    		ClockBonus2 timer4 = new ClockBonus2();
+    		timer4.print(false);
+    		timer4.addHours(25);
+    		timer4.print(false);
+    		timer4.addHours(-25);
+    		timer4.print(false);
+    		timer4.addMinutes(53);
+    		timer4.print(false);
+    		timer4.addMinutes(-53);
+    		timer4.print(false);
+    		timer4.addSeconds(7);
+    		timer4.print(false);
+    		timer4.addSeconds(-7);
+    		timer4.print(false);
+    		timer4.addMilliseconds(1020);
+    		timer4.print(false);
+    		timer4.addMilliseconds(-1020);
+    		timer4.print(false);
     		
-
+    		ClockBonus2 timer5 = new ClockBonus2(0,45,0,0);
+    		timer5.print(false);
+    		timer5.print(true);
     }
 }
--- src/ameedf/jb8223/assignments/hw02/ClockBonus2.java
+++ src/ameedf/jb8223/assignments/hw02/ClockBonus2.java
@@ -4,4 +4,68 @@ package ameedf.jb8223.assignments.hw02;
  * The same clock where minimum memory is occupied
  */
 public class ClockBonus2 {
+	private int milliseconds;
+
+	// C-tor initialize time to 14:07:53.980
+	public ClockBonus2() {
+		// Convert to milliseconds
+		this.milliseconds = (14 * 60 * 60 * 1000) + (7 * 60 * 1000) + (53 * 1000) + 980;
+		System.out.println("this.milliseconds: " + this.milliseconds);
+	}
+
+	// C-tor initialize time by user input
+	public ClockBonus2(int hours, int minutes, int seconds, int milliseconds) {
+		this.milliseconds = hours * 60 * 60 * 1000;
+		this.milliseconds += minutes * 60 * 1000;
+		this.milliseconds += seconds * 1000;
+		this.milliseconds += milliseconds;
+	}
+
+	public void addHours(int hours) {
+		//Check for out of range
+		hours /= 24; 
+		this.milliseconds += hours * 60 * 60 * 1000;
+	}
+
+	public void addMinutes(int minutes) {
+		this.milliseconds += minutes * 60 * 1000;
+	}
+	
+	public void addSeconds(int seconds) {
+		this.milliseconds += seconds * 1000;
+	}
+	
+	public void addMilliseconds(int milliseconds) {
+		this.milliseconds += milliseconds;
+	}
+
+	public void print(boolean is12) {
+		String postfix = "";
+		int tmp = this.milliseconds;
+
+		// Parse from milliseconds
+		int hours = tmp / (60 * 60 * 1000);
+		tmp %= (60 * 60 * 1000);
+		int minutes = tmp / (60 * 1000);
+		tmp %= (60 * 1000);
+		int seconds = tmp / 1000;
+		int milliseconds = tmp % 1000;
+
+		int hoursToShow = hours;
+		if (is12) {
+			if (hours < 12) {
+				if (hours == 0) {
+					hoursToShow = 12;
+				}
+				postfix = "AM";
+			} else {
+				postfix = "PM";
+				hoursToShow -= 12;
+			}
+		}
+		System.out.println("\n" + (hoursToShow < 10 ? "0" + hoursToShow : hoursToShow) + ":"
+				+ (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds) + "."
+				+ (milliseconds < 10 ? "00" + milliseconds : (milliseconds < 100 ? "0" + milliseconds : milliseconds))
+				+ " " + postfix);
+	}
 }
